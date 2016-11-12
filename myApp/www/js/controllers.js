82cf9247f10373eb964dd215de28e44034d3aee7
damvera <damian.vera.93@gmail.com>
2016-11-11 22:44:42 -0700

    Profile

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -1,6 +1,8 @@
 /* global angular, document, window */
 'use strict';
 
+var service_url = "http://damianvera93.esy.es/codecamp/";
+
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout) {
@@ -118,7 +120,7 @@ angular.module('starter.controllers', [])
     // Set Header
     $scope.$parent.showHeader();
     $scope.$parent.clearFabs();
-    $scope.isExpanded = false;
+    $scope.isExpanded = true;
     $scope.$parent.setExpanded(false);
     $scope.$parent.setHeaderFab(false);
 
@@ -190,6 +192,4 @@ angular.module('starter.controllers', [])
 
     // Activate ink for controller
     ionicMaterialInk.displayEffect();
-})
-
-;
+});
