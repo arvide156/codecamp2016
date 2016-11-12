fe02906ab5214ef963bc97342267fd21f254ab75
damvera <damian.vera.93@gmail.com>
2016-11-12 05:33:17 -0700

    GOOGLE MAPS!

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -270,7 +270,7 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('HomeCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+.controller('HomeCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion, $ionicLoading, $compile) {
     $scope.$parent.showHeader();
     $scope.$parent.clearFabs();
     $scope.isExpanded = false;
@@ -278,6 +278,92 @@ angular.module('starter.controllers', [])
     $scope.$parent.setHeaderFab('right');
     $scope.$parent.hideConfig();
     $scope.$parent.hideEdit();
+
+    function initialize() {
+
+        var locations = [
+            ["Pizza Pie Cafe", 37.116791, -113.544809],
+            ["Carls Jr", 37.110457, -113.554070],
+            ["Panda Express", 37.112540, -113.555329],
+            ["Papa John's Pizza", 37.108527, -113.561826]
+        ];
+
+        var centro_lat = 0;
+        var centro_lng = 0;
+
+        for(var i = 0; i < locations.length; i++){
+            centro_lat += locations[i][1];
+            centro_lng += locations[i][2];
+        }
+
+        centro_lat /= locations.length;
+        centro_lng /= locations.length;
+
+        var myLatlng = new google.maps.LatLng(centro_lat,centro_lng);
+        
+        var mapOptions = {
+          center: myLatlng,
+          zoom: 14,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map"),
+            mapOptions);
+
+        var infowindow = new google.maps.InfoWindow();
+
+            var marker, i;
+
+            for (i = 0; i < locations.length; i++) {  
+
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+                    map: map,
+                    icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
+                });
+
+                google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
+
+                    return function() {
+                        marker.setIcon("http://maps.google.com/mapfiles/ms/icons/blue-dot.png");
+                        infowindow.setContent(locations[i][0]);
+                        infowindow.open(map, marker);
+                    }
+
+                })(marker, i));
+
+                google.maps.event.addListener(marker, 'mouseout', (function(marker, i) {
+
+                    return function() {
+                        marker.setIcon("http://maps.google.com/mapfiles/ms/icons/red-dot.png");
+                        infowindow.close(map, marker);
+                    }
+
+                })(marker, i));
+            }
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+      
+      $scope.centerOnMe = function() {
+        if(!$scope.map) {
+          return;
+        }
+
+        $scope.loading = $ionicLoading.show({
+          content: 'Getting current location...',
+          showBackdrop: false
+        });
+
+        navigator.geolocation.getCurrentPosition(function(pos) {
+          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          $scope.loading.hide();
+        }, function(error) {
+          alert('Unable to get location: ' + error.message);
+        });
+      };
+      
+      $scope.clickTest = function() {
+        alert('Example of infowindow with ng-click')
+      };
 
     // Activate ink for controller
     ionicMaterialInk.displayEffect();
