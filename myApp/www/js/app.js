4d906f94f4ae21e87ec5193892221ef1b9ba022c
Mauricio Arvide <arvide156@Ritsu-Jr.local>
2016-11-11 22:32:33 -0700

    Removed the map stuff

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -146,37 +146,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'io
             }
         }
     })
-
-    .state('app.map', {
-        url: '/map',
-        views: {
-            'menuContent': {
-                templateUrl: 'templates/map.html',
-                controller: 'HomeCtrl'
-            },
-            'fabContent': {
-                template: '<button class="button button-fab button-stable" style="float: right; margin: 5px 50px" ng-click="openFilter()"><i class="icon ion-chevron-down"></i></button>',
-                controller: function ($scope, $timeout, $ionicModal) {
-                    $ionicModal.fromTemplateUrl('templates/filter.html', {
-                        scope: $scope,
-                        animation: 'slide-in-up'
-                    }).then(function(modal) {
-                        $scope.modal = modal;
-                    });
-                    $scope.openFilter = function() {
-                        $scope.modal.show();
-                    };
-                    $scope.closeFilter = function() {
-                        $scope.modal.hide();
-                    };
-
-                    $timeout(function () {
-                        document.getElementById('fab-filter').classList.toggle('on');
-                    }, 600);
-                }
-            }
-        }
-    })
     ;
 
     // if none of the above states are matched, use this as the fallback
